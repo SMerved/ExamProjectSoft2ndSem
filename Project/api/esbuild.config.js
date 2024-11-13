1e71abe6fea22f32109be2a7555580b595dd0769
SMerved <90383268+SMerved@users.noreply.github.com>
2024-11-13 11:59:28 +0100

    pls be fixed

---
+++ Project/api/esbuild.config.js
@@ -0,0 +1,12 @@
+import esbuild from 'esbuild';
+import {nodeExternalsPlugin}  from 'esbuild-node-externals';
+
+esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  outfile: './dist/bundle.js',
+  bundle: true,
+  loader: {
+    '.ts': 'ts', // Specify the TypeScript loader
+  },
+  plugins: [nodeExternalsPlugin()],
+}).catch(() => process.exit(1));
