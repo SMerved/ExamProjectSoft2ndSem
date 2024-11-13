89d467f12f89f5cc4bfdbc779167df3b8c8c0eec
SMerved <90383268+SMerved@users.noreply.github.com>
2024-11-13 12:01:04 +0100

    Update esbuild.config.js

--- Project/api/esbuild.config.js
+++ Project/api/esbuild.config.js
@@ -2,7 +2,7 @@ import esbuild from 'esbuild';
 import {nodeExternalsPlugin}  from 'esbuild-node-externals';
 
 esbuild.build({
-  entryPoints: ['./src/index.ts'],
+  entryPoints: ['./src/server.ts'],
   outfile: './dist/bundle.js',
   bundle: true,
   loader: {
